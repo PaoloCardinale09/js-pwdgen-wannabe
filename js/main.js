8977873cbcf7ba803bff9d93399f927058950d9b
PaoloCardinale09 <alfiozeronove@gmail.com>
2023-01-20 15:12:09 +0100

    add script

---
+++ js/main.js
@@ -0,0 +1,4 @@
+let user = prompt("Qual'è il tuo nome?");
+let surname = prompt("Qual'è il tuo cognome?");
+let favorite_color = prompt("Qaul'è il tuo colore preferito?");
+document.getElementById("password").innerHTML = ("La tua password è : " + user + surname + favorite_color + "23")
